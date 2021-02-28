451edf6ba65f7fb44e3cf5c740081075141ee4ab
achmadmiftahudin <78575410+achmadmiftahudin@users.noreply.github.com>
2021-03-01 06:28:26 +0700

    Delete websitepondok directory

--- websitepondok/script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.querySelector('.toggle input');
-const nav = document.querySelector('nav ul');
-
-toggle.addEventListener('click', function () {
-    nav.classList.toggle('slide');
-})
